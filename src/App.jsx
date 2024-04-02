9554f1826d261559e85d6b0d79a548adb93d5eeb
JeremyBaumiller <jbaumil07@gmail.com>
2024-04-02 17:12:39 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <a href="" target="_blank"> </a>
+        
+      <a href="" target="_blank">
+        
+      </a>
+    </div>
+  )
+}
+
+export default App
