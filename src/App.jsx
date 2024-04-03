d6205464838da0182a7548033b03840fb024e088
JeremyBaumiller <jbaumil07@gmail.com>
2024-04-03 00:27:24 -0400

    done with css background

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { puppyList } from './data.js'
 import './App.css'
 
+
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   console.log("puppyList: ", puppyList);
